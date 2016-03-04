add6d839f2b5155990451be77b3c6c5b7fa0f2ea
Khan <himan_sa@yahoo.com>
2016-03-04 11:37:26 -0500

    First Commit

---
+++ VistaDM.Repository/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VistaDM.Domain;
+
+namespace VistaDM.Repository
+{
+    public class UserRepository : BaseRepository
+    {
+        public UserRepository()
+        {
+        }
+
+       
+
+        public string[] GetRolesAsArray(string userName)
+        {
+            //return Entites.Users.First(u => u.Username == userName).Roles.Select(r => r.Name).ToArray();
+
+            List<string> cities = new List<string>();
+            cities.Add("New York");
+            cities.Add("Mumbai");
+            cities.Add("Berlin");
+            cities.Add("Istanbul");
+
+
+            return cities.ToArray();
+
+        }
+
+        
+    }
+}
